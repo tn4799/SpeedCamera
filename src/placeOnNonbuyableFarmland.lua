2b57d341f91aa88200023a598ec17f1707d16a42
tn4799 <tn4799@gmail.com>
2023-03-28 14:57:25 +0200

    added script to allow placement on non-buyable farmland

---
+++ src/placeOnNonbuyableFarmland.lua
@@ -0,0 +1,13 @@
+ConstructionBrushPlaceable.verifyAccess = Utils.overwrittenFunction(ConstructionBrushPlaceable.verifyAccess, function (self, superFunc, x, y, z)
+    if SpecializationUtil.hasSpecialization(PlaceableSpeedCamera, self.placeable.specializations) then
+        if not self:hasPlayerPermission() then
+            return ConstructionBrush.ERROR.NO_PERMISSION
+        elseif g_farmlandManager:getFarmlandIdAtWorldPosition(x, z) == FarmlandManager.NOT_BUYABLE_FARM_ID then
+            return nil
+        else
+            return superFunc(self, x, y, z)
+        end
+    else
+        return superFunc(self, x, y, z)
+    end
+end)
